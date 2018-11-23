bc7f2b289a799de0f079e02e922605de9784d539
liutiesong <cy@liutiesong.com>
2018-11-23 23:34:12 +0800

    first blod

---
+++ CYTools/CYTools/CYSystemTools.swift
@@ -0,0 +1,18 @@
+//
+//  CYSystemTools.swift
+//  CYTools
+//
+//  Created by 刘铁崧 on 2018/11/23.
+//  Copyright © 2018 刘铁崧. All rights reserved.
+//
+
+import UIKit
+
+class CYSystemTools: NSObject {
+    class func getSyStemInfoWithType()->String
+    {
+        // Do any additional setup after loading the view, typically from a nib.
+        var result:String = "123";
+        return result;
+    }
+}
---
+++ CYTools/CYTools/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CYTools
+//
+//  Created by 刘铁崧 on 2018/11/23.
+//  Copyright © 2018 刘铁崧. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(CYSystemTools.getSyStemInfoWithType());
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
